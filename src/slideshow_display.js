7f3d622a1669393be755e23ba1401e4c862b2874
selkari <raineydays5682@gmail.com>
2025-09-08 01:59:57 +1000

    add carousel of images

---
+++ src/slideshow_display.js
@@ -0,0 +1,29 @@
+let currSlide = 0;
+const buttonNext = document.getElementById('next-button');
+displayImg();
+
+buttonNext.addEventListener('click', () => {
+  nextSlide();
+});
+
+function nextSlide() {
+  images[currSlide].style.display = 'none';
+  currSlide++;
+  displayImg();
+}
+
+function displayImg() {
+  images = document.getElementsByClassName('slideshow-img');
+  console.log('changing image ', currSlide);
+  console.log(images.length);
+  if (currSlide < 0) {
+    currSlide = images.length - 1;
+  }
+  if (currSlide > (images.length - 1)) {
+    currSlide = 0;
+  }
+  for (img of images) {
+    img.style.display = 'none';
+  }
+  images[currSlide].style.display = 'block';
+}
